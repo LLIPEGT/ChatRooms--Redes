452ea5c401631c6141546cd61e42de265029752a
Fellipe Fernandes Nogueira <fellipe.5fernades@gmail.com>
2025-01-13 20:25:20 -0300

    Tela inicial de Grupo (index) - Feito

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./src/routes')
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(routes)
 
 const PORT = 8000;
