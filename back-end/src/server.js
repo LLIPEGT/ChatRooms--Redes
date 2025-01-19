e1af64ada954cf96ab515ef6d5cd94c110e535e1
Arthur Rodrigues <arthurrdgx@gmail.com>
2025-01-19 11:34:20 -0300

    chat feito, pendente sistema de salas

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,14 +3,10 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
+const wss = new WebSocketServer({ port: 8000 })
 
 wss.on("connection", (ws) => {
-    
     ws.on("error", console.error)
-
-    ws.send("mensagem enviada pelo servidor")
-
     ws.on("message", (data) => {
         console.log(data.toString())
         wss.clients.forEach((client) => client.send(data.toString()))
--- frontEnd/js/script.js
+++ frontEnd/js/script.js
@@ -9,6 +9,7 @@ const loginInput = login.querySelector(".login__input")
 const chat = document.querySelector(".chat")
 const chatForm = chat.querySelector(".chat__form")
 const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 const colors = [
     "blue",
@@ -25,16 +26,6 @@ const user = { id: "", name: "", color: "" }
 
 let websocket
 
-const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length)
-    return colors[randomIndex]
-} 
-
-const processMessage = ({data}) => {
-    alert(data)
-}
-
-
 const handleLogin = (event) => {
     event.preventDefault()
 
@@ -45,11 +36,69 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("ws://localhost:8000")
     // websocket.onopen = () =>  websocket.send(`usuario: ${user.name} logado`)
     websocket.onmessage = processMessage
-
-    console.log(user)
 }
 
-loginForm.addEventListener("submit",handleLogin)
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+    div.classList.add("message__self")
+    div.innerHTML = content
+
+    return div
+}
+
+const createMessageOtherElement = (content, sendername, sendercolor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+    div.classList.add("message__other")
+    div.classList.add("message__self")
+    span.classList.add("message__sender")
+    span.style.color = sendercolor
+    div.appendChild(span)
+    span.innerHTML = sendername
+    div.innerHTML += content
+
+    return div
+}
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+} 
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({data}) => {
+    const { userId, username, usercolor, content } = JSON.parse(data)
+    
+    const message = userId == user.id ? createMessageSelfElement(content) : createMessageOtherElement(content, username, usercolor)
+    
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
+
+const sendMessage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        username: user.name,
+        usercolor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message))
+    chatInput.value = ""
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit",sendMessage)
